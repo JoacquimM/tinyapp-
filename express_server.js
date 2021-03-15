54c4655520b7ef6d91f2b93d46c1c3d16fc888cb
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-15 22:39:14 +0000

    new files

--- express_server.js
+++ express_server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//const a = 15
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -14,6 +19,19 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  // const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 app.get("/jojo", (req, res) => {
   res.send("You're gonna get through this!");

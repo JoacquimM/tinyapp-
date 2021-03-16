27c18c62e238320258154d7b7e1279c90bbc8e8d
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-16 18:14:22 +0000

    new file updates

--- express_server.js
+++ express_server.js
@@ -23,6 +23,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+//----------------------------------------------------------------
+
+
+
 
 
 const urlDatabase = { // <---  use to keep track of all the URLs and their shortened forms. 
@@ -30,7 +34,9 @@ const urlDatabase = { // <---  use to keep track of all the URLs and their short
   "9sm5xK": "http://www.google.com"
 };
 
+shortURLdeletArray = []
 
+shortURLdeletArrayObj = {}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -68,10 +74,31 @@ app.post("/urls", (req, res) => {
   console.log(req.body);
   const longUrl = req.body.longURL
   let shortUrl = generateRandomString()  // Log the POST request body to the console
-    
+  shortURLdeletArray.push(shortUrl);  // <-- pushes newly created short Url to array(database) might not need
+  console.log(shortURLdeletArray);
   urlDatabase[shortUrl] = longUrl;
   res.redirect(`/urls/${shortUrl}`);     // Respond with 'Ok' (we will replace this)
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+
+  //  console.log(shortURLdeletArray.length - 1);
+   console.log(req.params.shortURL);
+   const shortURLDelete = req.params.shortURL;
+   delete urlDatabase[shortURLDelete]
+   res.redirect(`/urls`);
+ 
+});
+
+// app.delete("/urls/:shortURL/delete", function (req, res) {
+//   // res.send('DELETE request to homepage')
+//     const shortURLDelete = req.body.shortURL;
+//     delete urlDatabase[shortURLDelete];
+//   res.redirect(`/urls`);
+// })
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

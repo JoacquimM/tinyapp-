1e5415fd100146dc8deaefae931cd501e4b2ca06
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-16 01:14:01 +0000

    updates to file

--- express_server.js
+++ express_server.js
@@ -1,22 +1,36 @@
+
+function generateRandomString() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  let answer = "";
+  
+  for(let i = 0; i < 3; i ++){
+    let number = Math.floor(Math.random()*3);
+    let letter = alphabet[Math.floor(Math.random() * alphabet.length)]
+    answer += number;
+    answer += letter;
+  }
+  
+  console.log(answer);
+  return answer;
+  }
+//----------------------------------------------------------------
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; 
 
-// body parser below 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-//ejs
 app.set("view engine", "ejs");
 
-const generateRandomString = require("./randomString.js");
+
 
 const urlDatabase = { // <---  use to keep track of all the URLs and their shortened forms. 
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//const a = 15
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -45,9 +59,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+  const longUrl = req.body.longURL
+  let shortUrl = generateRandomString()  // Log the POST request body to the console
+    
+  urlDatabase[shortUrl] = longUrl;
+  res.redirect(`/urls/${shortUrl}`);     // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
